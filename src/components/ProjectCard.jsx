d3b205173f541111e18a4362d821d93994a645fd
Jordan <jordane.burton@gmail.com>
2024-05-13 14:28:38 -0500

    updated portfolio page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,26 +1,35 @@
 import { useState } from 'react';
+import { Row } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
-import { Row } from 'react-bootstrap';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
-function ProjectCard({ urlLinks, thumbnail, title }) {
+function ProjectCard({ urlLinks, thumbnail, title, thumbnailOpt, techStack }) {
     const [isHovered, setIsHovered] = useState(false);
     const [isHoveredGit, setIsHoveredGit] = useState(false);
+    const [isHoveredImg, setIsHoveredImg] = useState(false);
 
     const handleMouseEnter = () => {
       setIsHovered(true);
     };
     const handleMouseEnterGit = () => {
         setIsHoveredGit(true);
-      };
+    };
+    const handleMouseEnterImg = () => {
+        setIsHoveredImg(true);
+    };
 
     const handleMouseLeave = () => {
       setIsHovered(false);
     };
     const handleMouseLeaveGit = () => {
         setIsHoveredGit(false);
-      };
+    };
+    const handleMouseLeaveImg = () => {
+        setIsHoveredImg(false);
+    };
     
     function openLink() {
         window.open(urlLinks[0], '_blank').focus();
@@ -29,12 +38,31 @@ function ProjectCard({ urlLinks, thumbnail, title }) {
     function openRepo() {
         window.open(urlLinks[1], '_blank').focus();
     }
+
+    const technologies = (
+        <Popover id="popover-basic">
+            <Popover.Header>
+            <h5>
+                <strong>Tech Stack</strong>
+            </h5>
+            </Popover.Header>
+            {techStack.map((tech, index) => 
+                <Popover.Body>
+                    <h5>{tech}</h5>
+                </Popover.Body>
+            )}
+        </Popover>
+      );
     
     return (
         <>
             <Col xs={12} md={6} lg={4} className='project-card position-relative z-2 my-5'>
-                <Image src={thumbnail} thumbnail role='img' aria-label='Project thumbnail'/>
-                <svg onClick={() => { openRepo() }}
+                <div>
+                    <OverlayTrigger trigger="hover" placement="right" overlay={technologies}>
+                        <Image src={thumbnail} thumbnail={thumbnailOpt} role='img' aria-label='Project thumbnail'/>
+                    </OverlayTrigger>
+                </div>
+                {/* <svg onClick={() => { openRepo() }}
                     onMouseEnter={handleMouseEnterGit}
                     onMouseLeave={handleMouseLeaveGit}
                     style={{ 
@@ -43,7 +71,8 @@ function ProjectCard({ urlLinks, thumbnail, title }) {
                     }}
                     xmlns="http://www.w3.org/2000/svg" width="6rem" height="6rem" className="bi bi-github position-absolute top-50 start-50 translate-middle" viewBox="0 0 16 16" role="link" aria-label="Github link">
                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-                </svg>
+                </svg> */}
+
                 <div className='p-2 z-3'>
                     <h3 onClick={() => { openLink() }}
                         onMouseEnter={handleMouseEnter}
@@ -51,6 +80,18 @@ function ProjectCard({ urlLinks, thumbnail, title }) {
                         style={{ textDecorationLine: isHovered ? 'underline' : 'none' }
                     }
                     >{title}</h3>
+                    <h3 onClick={() => { openRepo() }}
+                        onMouseEnter={handleMouseEnterGit}
+                        onMouseLeave={handleMouseLeaveGit}
+                        style={{ textDecorationLine: isHoveredGit ? 'underline' : 'none' }}
+                    >Github 
+                        <span>
+                        <svg style={{ fill: 'white' }}
+                            xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" className="m-1 mb-2" viewBox="0 0 16 16" role="link" aria-label="Github link">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                        </svg>
+                        </span>
+                    </h3>
                 </div>
             </Col>
         </>
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -18,29 +18,46 @@ function PortfolioPage () {
 
   const projects = [
     {
+      urlLinks: ['https://jburton-java-api-1cf73a69112b.herokuapp.com/', 'https://github.com/jordaneburton/tech-news-java-api'],
+      thumbnail: '/assets/java-icon.png',
+      title: 'Java API',
+      thumbnailOpt: false,
+      techStack: ['Java', 'Spring Boot', 'Thymeleaf', 'MySQL']
+    },
+    {
       urlLinks: ['https://jordaneburton.github.io/recipe-finder/', 'https://github.com/jordaneburton/recipe-finder'],
-      thumbnail: '/assets/recipe-finder.png',
-      title: 'Online Cookbook'
+      thumbnail: '/assets/cooking-icon.png',
+      title: 'Online Cookbook',
+      thumbnailOpt: true,
+      techStack: ['Javascript', 'Bootstrap', 'RESTful API']
     },
     {
       urlLinks: ['https://tinder-for-jobs-53eebacc42f4.herokuapp.com/', 'https://github.com/jordaneburton/tinder-for-jobs'],
-      thumbnail: '/assets/tinder-for-jobs.png',
-      title: 'Tinder for Jobs'
+      thumbnail: '/assets/jobs-icon.png',
+      title: 'Tinder for Jobs',
+      thumbnailOpt: false,
+      techStack: ['Javascript', 'Handlebars.js', 'MySQL', 'RESTful API']
     },
     {
       urlLinks: ['https://jordaneburton.github.io/weather-dashboard/', 'https://github.com/jordaneburton/weather-dashboard'],
-      thumbnail: '/assets/weather-dashboard.png',
-      title: 'Weather Dashboard'
+      thumbnail: '/assets/weather-icon.png',
+      title: 'Weather Dashboard',
+      thumbnailOpt: false,
+      techStack: ['Javascript', 'Bootstrap', 'RESTful API']
     },
     {
       urlLinks: ['https://jordaneburton.github.io/quiz-game/', 'https://github.com/jordaneburton/quiz-game'],
-      thumbnail: '/assets/js-quiz-game.png',
-      title: 'Javascript Quiz Game'
+      thumbnail: '/assets/js-icon.png',
+      title: 'Javascript Quiz Game',
+      thumbnailOpt: false,
+      techStack: ['Javascript', 'HTML', 'CSS']
     },
     {
       urlLinks: ['https://github.com/jordaneburton/password-generator', 'https://jordaneburton.github.io/password-generator/'],
-      thumbnail: '/assets/password-generator.png',
-      title: 'Password Generator'
+      thumbnail: '/assets/password-icon.png',
+      title: 'Password Generator',
+      thumbnailOpt: false,
+      techStack: ['Javascript', 'HTML', 'CSS']
     },
   ];
 
@@ -48,10 +65,11 @@ function PortfolioPage () {
     <>
       <PageHeader header={page.header}/>
       <Row className='justify-content-evenly align-self-center page-margin'>
-        <h2>Try clicking the project titles!</h2>
+        <h2>This website portfolio itself is crafted using React and Bootstrap!</h2>
+        <h3>Hover over the project icons to show the tech stacks used. Click on the links below the icons to open the project's website or github page.</h3>
           {projects.map((project, index) => 
             <ProjectCard key={index}
-              urlLinks={project.urlLinks} thumbnail={project.thumbnail} title={project.title}>
+              urlLinks={project.urlLinks} thumbnail={project.thumbnail} title={project.title} thumbnailOpt={project.thumbnailOpt} techStack={project.techStack}>
             </ProjectCard>
           )}
       </Row>
